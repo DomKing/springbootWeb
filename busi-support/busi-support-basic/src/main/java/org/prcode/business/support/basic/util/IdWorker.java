e6db85eb1b7354eddc6a76284a916654cc027141
kangduo <beixianchen@gmail.com>
2017-11-15 17:25:48 +0800

    idWorker定时向redis续约

--- busi-support/busi-support-basic/src/main/java/org/prcode/business/support/basic/util/IdWorker.java
+++ busi-support/busi-support-basic/src/main/java/org/prcode/business/support/basic/util/IdWorker.java
@@ -7,6 +7,7 @@ import javax.annotation.PreDestroy;
 
 import org.apache.log4j.Logger;
 import org.prcode.utility.util.SequenceUtil;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
@@ -25,6 +26,11 @@ public class IdWorker {
     private static final int RAND_AREA = 2 << 4;
     private static final String ID_WORKER_CONFIG_PREFIX = "idWorker:config:";
 
+    /**
+     * 注册redis超时时间，避免因未能正确注销导致 workerId 一直不能用
+     */
+    private static final long EXPIRE_SECONDS = 15 * 60L;
+
     private static final Logger logger = Logger.getLogger(IdWorker.class);
 
     private static SequenceUtil getSequenceUtil() {
@@ -38,17 +44,32 @@ public class IdWorker {
             workerId = worker;
             dataCenterId = center;
             sequenceUtil = new SequenceUtil(workerId, dataCenterId);
-            RedisUtil.set(ID_WORKER_CONFIG_PREFIX + dataCenterId + "_" + workerId, "1");
+            RedisUtil.set(ID_WORKER_CONFIG_PREFIX + dataCenterId + "_" + workerId, "1", EXPIRE_SECONDS);
         }
         return sequenceUtil;
     }
 
+    /**
+     * 服务终止时，移除注册号
+     */
     @PreDestroy
     public void destroy() {
         if (dataCenterId != null && workerId != null) {
             String key = ID_WORKER_CONFIG_PREFIX + dataCenterId + "_" + workerId;
             logger.debug("服务终止，安全移除IdWorker在redis中的注册号：" + key);
             RedisUtil.del(key);
+        }
+    }
+
+    /**
+     * 定时向redis注册
+     */
+    @Scheduled(cron = "0 0/5 * * * * ")
+    public void regWorker() {
+        if (dataCenterId != null && workerId != null) {
+            String key = ID_WORKER_CONFIG_PREFIX + dataCenterId + "_" + workerId;
+            logger.debug("IdWorker服务进行中，向redis续约");
+            RedisUtil.set(key, "1", EXPIRE_SECONDS);
         }
     }
 
--- webOOS/src/main/java/org/prcode/WebOOSApplication.java
+++ webOOS/src/main/java/org/prcode/WebOOSApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * EnableCaching支持使用springCache
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableCaching
 @EnableAsync
+@EnableScheduling
 public class WebOOSApplication {
 
 	public static void main(String[] args) {
